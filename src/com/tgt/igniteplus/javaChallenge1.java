6c4d649ba28eb1b69004987792f98e76552ee70a
Charitha G <61057243+charitha-g@users.noreply.github.com>
2020-09-08 20:27:53 +0530

    Update javaChallenge1.java

--- src/com/tgt/igniteplus/javaChallenge1.java
+++ src/com/tgt/igniteplus/javaChallenge1.java
@@ -1,6 +1,7 @@
 package com.tgt.igniteplus;
 
 import java.time.LocalDateTime;
+//To print current date and time
 public class javaChallenge1 {
     public static void main(String[] args) {
         LocalDateTime a=LocalDateTime.now();
